47bb4e31d28bbaca6c2aeba5da0dc22b32cbeecc
Amber Brown <hawkowl@atleastfornow.net>
2018-08-01 08:11:04 +1000

    fix py36

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,9 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import utils
-
 from twisted.trial import util
+
+from tests import utils
 
 util.DEFAULT_TIMEOUT_DURATION = 10
 utils.setupdb()
